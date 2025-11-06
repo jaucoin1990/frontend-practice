6ca2211a074e44b81618c80c494547f2a3524017
jmaucoin1990 <jmaucoin90@gmail.com>
2025-11-06 14:54:43 -0800

    adding starter vite files and some test hero files

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './App.css';
+import Hero from './components/Hero';
+
+export default function App() {
+  return <div>
+    <Hero />
+  </div>
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Hero.css';
+
+export default function Hero () {
+    return <div id="hero">This is a Hero test</div>
+}
