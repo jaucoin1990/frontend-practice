0efe78d6b9a44ed3159b613e6182753fd8b3ef4a
jmaucoin1990 <jmaucoin90@gmail.com>
2025-11-06 15:26:27 -0800

    deleting hero pages

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './App.css';
-import Hero from './components/Hero';
-
-export default function App() {
-  return <div>
-    <Hero />
-  </div>
-}
--- src/components/Hero.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './Hero.css';
-
-export default function Hero () {
-    return <div id="hero">This is a Hero test</div>
-}
